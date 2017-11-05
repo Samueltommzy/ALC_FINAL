d244f63f7236c9fbe3a40b3b9125e2d43a7d248a
Samuel <samueloluwatommzy@gmail.com>
2017-11-05 12:09:05 +0100

    entry point for nodejs app

---
+++ app.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const { database, databaseName } = require('./config/database');
+const cors = require('cors');
+
+let app = express();
+
+let http = require("http").Server(app);
+let socket_io = require('socket.io')(http);
+
+app.use(cors());
+
+mongoose.connect(database, function(err) {
+    if (err) {
+        console.log("connection to database failed");
+    } else {
+        console.log(" Connected to database " + databaseName);
+    }
+});
+
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+//app.use('/scripts', express.static(`${__dirname }/node_modules`))
+
+let api = require("./app/api")(app, express, socket_io);
+
+app.use("/api", api);
+
+http.listen(port, function(err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("app listening on port " + port);
+    }
+// <<<<<<< HEAD
+// =======
+});
+
+app.use((err, request, response, next) => {
+    response.status(500).send({
+        status: 500,
+        success: false,
+        message: `Something went wrong`,
+        errorMessage: err.message,
+        errorCode: err.errorCode
+    });
+    return false;
+// >>>>>>> 216abc35ea3b50a8adb270c33b38511524f0edc3
+});
